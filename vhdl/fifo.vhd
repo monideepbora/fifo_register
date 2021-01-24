6f57e00d45eb0cfe2d91bd3af803573a1390feb4
monideepbora <monideepbora@gmail.com>
2021-01-24 22:27:39 +0100

    a few edits

--- vhdl/fifo.vhd
+++ vhdl/fifo.vhd
@@ -1,3 +1,7 @@
+-- Description : FIFO Entity 
+-- Author : Monideep Bora
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
--- vhdl/fifo_tb.vhd
+++ vhdl/fifo_tb.vhd
@@ -1,3 +1,6 @@
+-- Description : FIFO Entity Testbench 
+-- Author : Monideep Bora
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
